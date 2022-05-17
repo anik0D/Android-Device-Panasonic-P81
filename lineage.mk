570b1647da6941dc4b384ff2ec6d8d331dbdecbd
anik0D <73814243+anik0D@users.noreply.github.com>
2022-05-17 16:02:51 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,11 +1,11 @@
 # Device path
-DEVICE_PATH := device/Panasonic/P55Novo
+DEVICE_PATH := device/Panasonic/P81
 
 # Vendor path
-VENDOR_PATH := vendor/Panasonic/P55Novo
+VENDOR_PATH := vendor/Panasonic/P81
 
 # Release name
-PRODUCT_RELEASE_NAME := P55Novo
+PRODUCT_RELEASE_NAME := P81
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -18,8 +18,8 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := P55Novo
-PRODUCT_NAME := lineage_P55Novo
+PRODUCT_DEVICE := P81
+PRODUCT_NAME := lineage_P81
 PRODUCT_BRAND := Panasonic
-PRODUCT_MODEL := Panasonic P55 Novo by Shivom Techdroid
+PRODUCT_MODEL := Panasonic P81
 PRODUCT_MANUFACTURER := MediaTek
